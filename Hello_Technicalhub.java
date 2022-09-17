1dd18abeaeae4c848aa4bc7a36bd54d335be364c
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-09-17 20:31:53 +0530

    import java.util.Scanner; class hello {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         String c=s.nextLine();         System.out.format("Hello Technicalhub ");         System.out.format("%s ",c);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class hello
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String c=s.nextLine();
+        System.out.format("Hello Technicalhub
+");
+        System.out.format("%s
+",c);
+    }
+}
