f6da909910b2985db8b46907f3ddab9e7ed4cce9
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-09-19 22:13:47 +0530

    import java.util.Scanner; class sum {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int[] a=new int[n];         float avg;         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         int c=0;         for(int i=0;i<n;i++)         {             c+=a[i];         }         avg=(float)c/n;         System.out.format("%.2f",avg);     } }

---
+++ average_of_the_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        float avg;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            c+=a[i];
+        }
+        avg=(float)c/n;
+        System.out.format("%.2f",avg);
+    }
+}
