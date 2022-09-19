7989b8838eefcd40af24d054896ca4a3096e822e
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-09-19 23:28:43 +0530

    import java.util.Scanner; class digits {     public static int p(int n)     {         int i;         if(n==1)         {             return 0;         }         for(i=2;i<=(n/2);i++)         {            if(n%i==0)             {                return 0;             }         }         return 1;     }     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         int k=s.nextInt();         int c=0;         for(int i=0;i<n;i++)         {             if(p(a[i])==1&&a[i]>=k)             {                 c++;             }         }         System.out.format("%d",c);     } }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class digits
+{
+    public static int p(int n)
+    {
+        int i;
+        if(n==1)
+        {
+            return 0;
+        }
+        for(i=2;i<=(n/2);i++)
+        {
+           if(n%i==0)
+            {
+               return 0;
+            }
+        }
+        return 1;
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        int k=s.nextInt();
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(p(a[i])==1&&a[i]>=k)
+            {
+                c++;
+            }
+        }
+        System.out.format("%d",c);
+    }
+}
