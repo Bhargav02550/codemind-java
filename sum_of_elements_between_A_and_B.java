8cd050c0148c07759ed7bbe670f391f05f26e9ef
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-09-20 12:19:04 +0530

    import java.util.Scanner; class character {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         int c=s.nextInt();         int d=s.nextInt();         int sum=0;         for(int i=0;i<n;i++)         {             if(a[i]>=c && a[i]<=d)             {                 sum+=a[i];             }         }         System.out.format("%d",sum);     } }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class character
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        int c=s.nextInt();
+        int d=s.nextInt();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>=c && a[i]<=d)
+            {
+                sum+=a[i];
+            }
+        }
+        System.out.format("%d",sum);
+    }
+}
