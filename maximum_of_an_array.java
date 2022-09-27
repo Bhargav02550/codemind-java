854318d8f291c01a8fc4e30f2dc92995e88d3cfe
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-09-27 22:08:40 +0530

    import java.util.Scanner; class avg {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         int min=a[0];         for(int i=0;i<n;i++)         {             if(a[i]>min)             {                 min=a[i];             }         }         System.out.format("%d",min);     } }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class avg
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        int min=a[0];
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>min)
+            {
+                min=a[i];
+            }
+        }
+        System.out.format("%d",min);
+    }
+}
