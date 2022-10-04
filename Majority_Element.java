0d1e44fc54cc9b159f4022a14e77db50e1b55b48
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-10-04 12:14:27 +0530

    import java.util.*; class solution {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         int c=0;         for(int i=0;i<n;i++)         {             c=0;             for(int j=0;j<n;j++)             {                 if(i!=j && a[i]==a[j])                 {                     c++;                 }             }             if(c>=(n/2))             {                 System.out.format("%d",a[i]);                 break;             }         }     } }

---
+++ Majority_Element.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class solution
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j && a[i]==a[j])
+                {
+                    c++;
+                }
+            }
+            if(c>=(n/2))
+            {
+                System.out.format("%d",a[i]);
+                break;
+            }
+        }
+    }
+}
