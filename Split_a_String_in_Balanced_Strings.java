51a38719be7def59e151bf10fdab140aaade2c44
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-11-02 06:25:15 +0530

    import java.util.*;
    class solution
    {
    	public static void main(String[] args) 
    	{
    		Scanner sc=new Scanner(System.in);
    		String s=sc.next();
    		int n=0,c=0;
    		for(int i=0;i<s.length();i++)
    		{
    		    if(s.charAt(i)=='R')
    		    n++;
    		    else
    		    n--;
    		    if(n==0)
    		    c++;
    		}
    		System.out.println(c);
    	}
    }

---
+++ Split_a_String_in_Balanced_Strings.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class solution
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		int n=0,c=0;
+		for(int i=0;i<s.length();i++)
+		{
+		    if(s.charAt(i)=='R')
+		    n++;
+		    else
+		    n--;
+		    if(n==0)
+		    c++;
+		}
+		System.out.println(c);
+	}
+}
