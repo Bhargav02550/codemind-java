1362c9246c345b82025216b2d103b7d98de80edb
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-11-08 21:24:05 +0530

    import java.util.*; class solution {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n-2;j++)             {                 System.out.format("%d",j);             }             for(int k=1;k<=n-3;k++)             {                 System.out.format("%d",k);             }             System.out.format(" ");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -5,11 +5,15 @@ class solution
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        for(int i=n;i>=1;i--)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=n;j>=1;j--)
+            for(int j=1;j<=n-2;j++)
             {
-                System.out.format("%d ",j);
+                System.out.format("%d",j);
+            }
+            for(int k=1;k<=n-3;k++)
+            {
+                System.out.format("%d",k);
             }
             System.out.format("
 ");
